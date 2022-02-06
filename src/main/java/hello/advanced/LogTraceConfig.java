894b9b05a357d4389c579cdf411922225ff00791
injinjeong <snowwwwww@naver.com>
2022-02-06 12:24:40 +0900

    ThreadLocal 사용시에는 반드시 사용 후 제거해야 한다.

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -1,15 +1,26 @@
-package hello.advanced.trace;
+package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * ThreadLocal 의 주의사항 :
+ * 사용 후 반드시 제거해야 한다.
+ */
 @Configuration
 public class LogTraceConfig {
 
+//    @Bean
+//    public LogTrace logTrace() {
+//        return new FieldLogTrace();
+//    }
+
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
