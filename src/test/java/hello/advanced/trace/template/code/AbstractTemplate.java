3a6095c49f2372bdf2244bde335b8018b9dd17f8
injinjeong <snowwwwww@naver.com>
2022-02-06 17:39:48 +0900

    template method pattern @Ovveride 를 이용한 패턴 적용

--- src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
+++ src/test/java/hello/advanced/trace/template/code/AbstractTemplate.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class AbstractTemplate {
 
-    public void excute() {
+    public void execute() {
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
         call(); // 상속
