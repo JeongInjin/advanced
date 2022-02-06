bf7f576a3676cab3cb4b9a7fb5c8ec7ab98aa757
injinjeong <snowwwwww@naver.com>
2022-02-06 10:23:20 +0900

    필드동기화 FieldLogTrace add

---
+++ src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,30 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    void begin_end_lever2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_lever2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+
+        trace.exceotion(status2, new IllegalStateException());
+        trace.exceotion(status1, new IllegalStateException());
+    }
+
+}
