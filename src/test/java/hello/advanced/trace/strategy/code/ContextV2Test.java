60b4376c2961d04587fa77b080ef55ca57e7535b
injinjeong <snowwwwww@naver.com>
2022-02-08 21:20:46 +0900

    전략패턴 3 - 전략을 팔아미터로 전달 받는 방식 추가

---
+++ src/test/java/hello/advanced/trace/strategy/code/ContextV2Test.java
@@ -0,0 +1,48 @@
+package hello.advanced.trace.strategy.code;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV2;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     * 전략 패턴 적용 - 전략을 파라미터로 전달 받는 방식
+     */
+
+    @Test
+    void strategyV1() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(new StrategyLogic1());
+        contextV2.execute(new StrategyLogic2());
+    }
+
+    @Test
+    void strategyV2() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        contextV2.execute(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV2 contextV2 = new ContextV2();
+        contextV2.execute(() -> log.info("비즈니스 로직1 실행"));
+        contextV2.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+
+}
